0244c848369713bd189cd6d4bdcb9d69f40cb8b7
Larisa Markova <lovemirror@rambler.ru>
2026-07-28 16:43:20 +0300

    docs(BadgeDot): unify story backgrounds

--- src/components/BadgeDot/stories/BadgeDotPlayground.template.tsx
+++ src/components/BadgeDot/stories/BadgeDotPlayground.template.tsx
@@ -1,5 +1,11 @@
 import { BadgeDot, type BadgeDotProps } from '@admiral-ds/admiral3-primitives';
 
+import { StoryDemoContainer } from '../../stories/StoryContainers';
+
 export const BadgeDotPlaygroundTemplate = (args: BadgeDotProps) => {
-  return <BadgeDot {...args} />;
+  return (
+    <StoryDemoContainer>
+      <BadgeDot {...args} />
+    </StoryDemoContainer>
+  );
 };
